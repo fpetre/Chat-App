3a0c1ac452c6061c5cab047efafecbd040893248
Florin Petre <fmihailpetre@gmail.com>
2014-12-17 19:37:05 -0500

    display and update list of users in chat

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -4,6 +4,7 @@ $( function(){
 	var chat = new Chat(socket);
 	var $form = $("form");
 	var nickname = "";
+	var userList = [];
 
 	var getMessage = function () {
 		return $form.find("input#messages").val();
@@ -27,6 +28,26 @@ $( function(){
 		$form.find("input#messages").val("");
 	};
 
+	var renderUserList = function () {
+		$("#user-list").empty();
+		for (var i = 0; i < userList.length; i++) {
+			var userElement = $("<li id=" + userList[i] + ">" + userList[i] + "</li>");
+			$("#user-list").append(userElement);
+		}
+	};
+
+	var updateUserList = function (users) {
+		var newUserList = [];
+		for (var prop in users ) {
+			if(!users.hasOwnProperty(prop)){
+				continue;
+			}
+			newUserList.push(users[prop]);
+		}
+
+		userList = newUserList;
+	};
+
 
 	socket.on('nicknameChangeResult', function (data){
 		if (data.success === true) {
@@ -35,6 +56,11 @@ $( function(){
 		}
 			$("#received-messages").text(data.message);
 			$(".sender").text("sent from server");
+	});
+
+	socket.on("nicknameChangeUpdate", function (data) {
+		updateUserList(data.users);
+		renderUserList();
 	});
 
 	socket.on('sendmessage', function (data) {
@@ -49,10 +75,23 @@ $( function(){
 	});
 
 	socket.on('userDisconnect', function (data){
-
 		$("#received-messages").text(data.user + " has left the chat");
 		$(".sender").text("sent from server");
 	});
+
+	socket.on('addUser', function (data){
+		updateUserList(data.users);
+		renderUserList();
+	});
+
+
+
+	socket.on('removeUser', function (data){
+		updateUserList(data.users);
+		renderUserList();
+	});
+
+
 
 	$form.on("submit",function(event){
 		event.preventDefault();
